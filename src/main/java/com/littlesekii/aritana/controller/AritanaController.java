0735b0dffee4a22ff89bd80c3eee56055a6cc247
littlesekii <littlesekii@gmail.com>
2023-09-19 19:53:34 -0300

    Added integration with Aritana Library

--- src/main/java/com/littlesekii/aritana/controller/AritanaController.java
+++ src/main/java/com/littlesekii/aritana/controller/AritanaController.java
@@ -1,12 +1,18 @@
 package com.littlesekii.aritana.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lib.littlesekii.aritana.Aritana;
+
 @RestController
 @RequestMapping
 public class AritanaController {
+
+    @Value("${api.version}")
+    private String apiVersion;
     
     @GetMapping("/")
     public String status() {
@@ -16,5 +22,15 @@ public class AritanaController {
     @GetMapping("/api")
     public String api() {
         return "This is a Aritana model. Please stay tuned.";
+    }
+
+    @GetMapping("/api/version")
+    public String apiVersion() {        
+        return apiVersion;
+    }
+    
+    @GetMapping("/api/lib/version")
+    public String libVersion() {
+        return Aritana.version();
     }
 }

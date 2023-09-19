0e90d7f5f9bd088528f75fce3a6cf3c45526bd6f
littlesekii <littlesekii@gmail.com>
2023-09-19 10:14:13 -0300

    Project started

---
+++ src/main/java/com/littlesekii/aritana/AritanaApplication.java
@@ -0,0 +1,13 @@
+package com.littlesekii.aritana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AritanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AritanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/littlesekii/aritana/AritanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littlesekii.aritana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AritanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
